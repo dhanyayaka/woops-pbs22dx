ebb0328a3123f51e4412021c2ce3fcb2eaf2a0de
Claraindriani1764 <claraindriani1764@gmail.com>
2025-05-24 23:09:54 +0700

    clara: menambahkan gambar di tampilan content

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,14 +5,15 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-3xl font-bold text-center my-4">Selamat Datang di E-Ruang UTI</h1>
-      <p className="text-lg font-italic text-center italic">Platform Digital Peminjaman Ruangan Universitas Teknokrat Indonesia</p>
+      <p className="text-lg  italic text-center">Platform Digital Peminjaman Ruangan Universitas Teknokrat Indonesia</p>
       <div className="flex justify-center items-center mt-8">
         <Image
-          src="/images/ruangan.png"
+          src="/images/gambar1.png"
           alt="Gambar 1"
-          width={500}
-          height={300}
-          layout="fixed"
+          width={1800}
+          height={500}
+          priority
+          className="object-contain"
         />
       </div>
     </div>
