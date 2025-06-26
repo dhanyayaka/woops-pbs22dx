bd989251f90c504840068fd71130ca2f22db9811
Claraindriani1764 <claraindriani1764@gmail.com>
2025-06-26 23:53:18 +0700

    clara: membuat tombol button simpan  menjadi hover jika di klik

--- frontend/app/pruang/page.tsx
+++ frontend/app/pruang/page.tsx
@@ -186,10 +186,10 @@ export default function AddPeminjaman() {
           <div className="flex justify-end gap-3 mt-8">
             <button
               type="submit"
-              className="w-32 py-2 px-4 rounded-lg border-2 border-green-500 text-green-700 font-semibold bg-white hover:bg-green-50 hover:border-green-600 transition-colors duration-200 shadow-sm flex items-center justify-center gap-2"
+              className="w-32 py-2 px-4 rounded-lg border-2 border-green-500 text-green-700 font-semibold bg-white hover:bg-green-700 hover:border-green-800 hover:text-white active:bg-green-900 transition-colors duration-200 shadow-sm flex items-center justify-center gap-2"
             >
               <svg
-              className="w-5 h-5 text-green-500"
+              className="w-5 h-5 text-green-500 group-hover:text-white"
               fill="none"
               stroke="currentColor"
               strokeWidth={2}
