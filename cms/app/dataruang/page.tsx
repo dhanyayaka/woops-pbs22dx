62fd6b170cb656103f2100a0cf282d572cd38603
Faniptricia <fanipatricia03@gmail.com>
2025-06-26 16:01:01 +0700

    Fani: Tambahkan tabel tampilan data ruangan

--- cms/app/dataruang/page.tsx
+++ cms/app/dataruang/page.tsx
@@ -44,7 +44,34 @@ export default function DataRuangPage() {
 ) : data.length === 0 ? (
   <p className="text-center text-gray-500">Tidak ada data ruangan.</p>
 ) : (
-  
+  <div className="overflow-x-auto">
+  <table className="w-full table-auto border-collapse text-sm rounded-lg overflow-hidden">
+    <thead>
+      <tr className="bg-gradient-to-r from-yellow-300 to-yellow-400 text-white text-center uppercase tracking-wide text-xs font-semibold">
+        <th className="py-4 px-4 text-black">Nama Ruangan</th>
+        <th className="py-4 px-4 text-black">Kapasitas</th>
+        <th className="py-4 px-4 text-black">Keterangan</th>
+        <th className="py-4 px-4 text-black">Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((item, index) => (
+        <tr
+          key={item.id}
+          className={`text-center ${
+            index % 2 === 0 ? "bg-yellow-50" : "bg-white"
+          } hover:bg-yellow-100 transition-colors duration-200`}
+        >
+          <td className="py-3 px-4 text-gray-800 font-medium">{item.namaRuangan}</td>
+          <td className="py-3 px-4 text-gray-800 font-medium">{item.kapasitas}</td>
+          <td className="py-3 px-4 text-gray-800 font-medium">{item.keterangan}</td>
+          <td className="py-3 px-4 text-gray-800 font-medium">
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
 )}
     </div>
     
