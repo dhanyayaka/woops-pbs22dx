c33b099cff0059e2b151d684b1592118c8228ce4
Faniptricia <fanipatricia03@gmail.com>
2025-06-26 15:59:14 +0700

    Fani: Menambahkan fetch API ruangan menggunakan useEffect

--- cms/app/dataruang/page.tsx
+++ cms/app/dataruang/page.tsx
@@ -12,6 +12,28 @@ export default function DataRuangPage() {
   const [data, setData] = useState<Ruangan[]>([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchRuangan = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/api/ruangan", {
+          cache: "no-store",
+        });
+        const result = await res.json();
+  
+        if (res.ok && result.data_ruangan) {
+          setData(result.data_ruangan);
+        } else {
+          console.error(result.metaData?.message || "Terjadi kesalahan");
+        }
+      } catch (error) {
+        console.error("Gagal mengambil data ruangan:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchRuangan();
+  }, []);
   return (
     <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-xl p-8 mt-12 border border-yellow-200">
       <h2 className="text-right text-3xl font-bold text-yellow-700 mb-6">
