f28cbcb1ca86ae33104c3cb80710309704f66dc2
Claraindriani1764 <claraindriani1764@gmail.com>
2025-06-26 23:56:04 +0700

    clara: update judul inputan form

--- frontend/app/pruang/page.tsx
+++ frontend/app/pruang/page.tsx
@@ -80,7 +80,7 @@ export default function AddPeminjaman() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-50">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg">
-        <h1 className="text-2xl font-bold mb-6 text-center">Tambah Data Peminjaman</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center">Masukan Data Peminjam</h1>
         <form
           onSubmit={e => {
             e.preventDefault();
