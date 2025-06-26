f74a8479124946a579f1c5cbfda4b5623c91c195
Faniptricia <fanipatricia03@gmail.com>
2025-06-26 15:31:14 +0700

    Fani: Tambah fetch API dan tampilkan loading

--- cms/app/datapeminjam/page.tsx
+++ cms/app/datapeminjam/page.tsx
@@ -1,12 +1,27 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function DataPeminjamPage() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/peminjaman")
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.data_user || []);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div className="max-w-7xl mx-auto p-8 mt-12">
       <h2 className="text-2xl font-bold">Data Peminjam</h2>
+      {loading ? <p>Loading data...</p> : <div>Data berhasil dimuat</div>}
     </div>
   );
 }
