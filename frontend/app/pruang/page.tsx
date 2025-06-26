cd3344000895dd23807d52b9f3895d7a77ebd64d
Claraindriani1764 <claraindriani1764@gmail.com>
2025-06-26 20:34:16 +0700

    clara: update Fungsi untuk menampilkan pesan error jika form input tidak di isi

--- frontend/app/pruang/page.tsx
+++ frontend/app/pruang/page.tsx
@@ -45,57 +45,43 @@ export default function AddPeminjaman() {
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Nama Peminjam</legend>
         <input type="text" className="input" placeholder="Isi Nama Peminjam" />
-        {errors.namaPeminjam && (
-          <span className="text-red-500 text-xs">Nama Peminjam tidak boleh kosong</span>
-        )}
+        {errors.namaPeminjam && <p className="label text-red-700">Nama Peminjam harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">NPM</legend>
         <input type="text" className="input" placeholder="Isi NPM" />
-        {errors.npm && (
-          <span className="text-red-500 text-xs">NPM tidak boleh kosong</span>
-        )}
+        {errors.npm && <p className="label text-red-700">NPM harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Nama Ruangan</legend>
         <input type="text" className="input" placeholder="Isi Nama Ruangan" />
-        {errors.namaRuangan && (
-          <span className="text-red-500 text-xs">Nama Ruangan tidak boleh kosong</span>
-        )}
+        {errors.namaRuangan && <p className="label text-red-700">Nama Ruangan harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Tanggal Peminjaman</legend>
         <input type="date" className="input" />
-        {errors.tanggalPeminjam && (
-          <span className="text-red-500 text-xs">Tanggal Peminjaman tidak boleh kosong</span>
-        )}
+        {errors.tanggalPeminjam && <p className="label text-red-700">Tanggal Peminjaman harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Waktu Mulai</legend>
         <input type="time" className="input" />
-        {errors.waktuMulai && (
-          <span className="text-red-500 text-xs">Waktu Mulai tidak boleh kosong</span>
-        )}
+        {errors.waktuMulai && <p className="label text-red-700">Waktu Mulai harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Waktu Akhir</legend>
         <input type="time" className="input" />
-        {errors.waktuAkhir && (
-          <span className="text-red-500 text-xs">Waktu Akhir tidak boleh kosong</span>
-        )}
+        {errors.waktuAkhir && <p className="label text-red-700">Waktu Akhir harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Keterangan</legend>
         <input  type="text" className="input" placeholder="Isi Keterangan" />
-        {errors.keterangan && (
-          <span className="text-red-500 text-xs">Keterangan tidak boleh kosong</span>
-        )}
+        {errors.keterangan && <p className="label text-red-700">Keterangan harus diisi</p>}
       </fieldset>
 
       <section className="mt-5">
