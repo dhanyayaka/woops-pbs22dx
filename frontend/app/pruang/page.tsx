097fde12ed09b4708a73ac0e9d317c2bac55e18b
Claraindriani1764 <claraindriani1764@gmail.com>
2025-06-26 23:54:34 +0700

    clara: membuat tombol button batal  menjadi hover jika di klik

--- frontend/app/pruang/page.tsx
+++ frontend/app/pruang/page.tsx
@@ -202,10 +202,10 @@ export default function AddPeminjaman() {
             <button
               type="button"
               onClick={setReload}
-              className="w-32 py-2 px-4 rounded-lg border-2 border-red-500 text-red-700 font-semibold bg-white hover:bg-red-50 hover:border-red-600 transition-colors duration-200 shadow-sm flex items-center justify-center gap-2"
+              className="w-32 py-2 px-4 rounded-lg border-2 border-red-500 text-red-700 font-semibold bg-white hover:bg-red-700 hover:border-red-800 hover:text-white active:bg-red-900 transition-colors duration-200 shadow-sm flex items-center justify-center gap-2 group"
             >
               <svg
-              className="w-5 h-5 text-red-500"
+              className="w-5 h-5 text-red-500 group-hover:text-white"
               fill="none"
               stroke="currentColor"
               strokeWidth={2}
