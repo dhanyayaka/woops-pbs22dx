3048aa45f897c85a08ba5776d54ff396f2e605f6
Claraindriani1764 <claraindriani1764@gmail.com>
2025-06-26 20:37:33 +0700

    clara: menarik fungsi useRef ke dalam form inputan

--- frontend/app/pruang/page.tsx
+++ frontend/app/pruang/page.tsx
@@ -44,43 +44,43 @@ export default function AddPeminjaman() {
       <title>Tambah Data Peminjaman</title>
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Nama Peminjam</legend>
-        <input type="text" className="input" placeholder="Isi Nama Peminjam" />
+        <input ref={namaPeminjamRef} type="text" className="input" placeholder="Isi Nama Peminjam" />
         {errors.namaPeminjam && <p className="label text-red-700">Nama Peminjam harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">NPM</legend>
-        <input type="text" className="input" placeholder="Isi NPM" />
+        <input ref={npmRef} type="text" className="input" placeholder="Isi NPM" />
         {errors.npm && <p className="label text-red-700">NPM harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Nama Ruangan</legend>
-        <input type="text" className="input" placeholder="Isi Nama Ruangan" />
+        <input ref={namaRuanganRef} type="text" className="input" placeholder="Isi Nama Ruangan"/>
         {errors.namaRuangan && <p className="label text-red-700">Nama Ruangan harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Tanggal Peminjaman</legend>
-        <input type="date" className="input" />
+        <input ref={tanggalPeminjamRef} type="date" className="input" />
         {errors.tanggalPeminjam && <p className="label text-red-700">Tanggal Peminjaman harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Waktu Mulai</legend>
-        <input type="time" className="input" />
+        <input ref={waktuMulaiRef} type="time" className="input" />
         {errors.waktuMulai && <p className="label text-red-700">Waktu Mulai harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Waktu Akhir</legend>
-        <input type="time" className="input" />
+        <input ref={waktuAkhirRef} type="time" className="input" />
         {errors.waktuAkhir && <p className="label text-red-700">Waktu Akhir harus diisi</p>}
       </fieldset>
 
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-[14px]">Keterangan</legend>
-        <input  type="text" className="input" placeholder="Isi Keterangan" />
+        <input ref={keteranganRef} type="text" className="input" placeholder="Isi Keterangan" />
         {errors.keterangan && <p className="label text-red-700">Keterangan harus diisi</p>}
       </fieldset>
 
